1f6d58a759aeeca051d14ac6a4aedc6d5a7c1dea
LeilaGroves <lgroves@calpoly.edu>
2024-02-19 23:58:51 -0800

    Task 1

--- task1/task1_tests.c
+++ task1/task1_tests.c
@@ -11,7 +11,25 @@ void test1() {
    checkit_string(result, expected);
 }
 
+void test2() {
+	char input[] = "Another TEST  ";
+	char result[16];
+	char *expected = "another test  ";
+
+	str_lower(input, result);
+	checkit_string(result, expected);
+}
+void test3() {
+	char input[] = "FoR the OthER ";
+	char *expected = "for the other ";
+
+	str_lower_mutate(input);
+	checkit_string(input, expected);
+}
+
 int main(void) {
    test1();
+   test2(); 
+   test3();
    return 0;
 }
